9c9d541add7b5e6b473197ed6e7c3c18c6ee6c0f
Aaron Teo <aaron.teo@riv-alumni.com>
2022-03-01 18:57:36 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-pip install -r requirements.txt
+if [[ "$OSTYPE" =~ ^msys ]]; then
+  py -3 -m pip install --upgrade pip
+  py -3 -m pip install -r requirements.txt
+else
+  python3 -m pip install --upgrade pip
+  python3 -m pip install -r requirements.txt
+fi
